acb6627644ff25fd7adf818ed89cde6cb58cd744
makarinua <makarinua@gmail.com>
2024-01-10 17:14:11 +0200

    hw1_js_10012024

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ function getElementWidth(content, padding, border) {
     const contentWidth = Number.parseFloat(content);
     const paddingWidth = Number.parseFloat(padding) * 2;
     const borderWidth = Number.parseFloat(border) * 2;
-    console.log(contentWidth + paddingWidth + borderWidth);    
+    return console.log(contentWidth + paddingWidth + borderWidth);    
 }
 
 console.log(getElementWidth("50px", "8px", "4px"));
